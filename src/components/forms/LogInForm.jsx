b049ee096473c47cfc149375e4a9bd5f07fb0dec
Laura Gil <laura.gilcara@gmail.com>
2024-09-06 22:38:25 +0200

    add log in

---
+++ src/components/forms/LogInForm.jsx
@@ -0,0 +1,79 @@
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { requestHandler } from "../../services/requestHandler";
+import { URL_API_LOGIN } from "../../config/urls";
+import Button from "../buttons/Button";
+import TextInput from "./inputs/TextInput";
+
+const LogInForm = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const navigate = useNavigate();
+
+    const placeholders = {
+        name: "Introduce el nombre del perfil principal...",
+        email: "Introduce tu email...",
+        password: "Introduce tu contraseña...",
+        passConf: "Introduce de nuevo la contraseña...",
+    };
+
+    const onSubmit = async (data) => {
+        const user = {
+            email: data.email.toLowerCase(),
+            password: data.password,
+        };
+
+        try {
+            const data = await requestHandler(URL_API_LOGIN, "POST", user);
+            console.log(data);
+            navigate("/");
+        } catch (error) {
+            console.log(`Error: ${error.message}`);
+        }
+    };
+    return (
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-8 bg-primary bg-opacity-[0.5] px-[1.625rem] pt-8 pb-[3.75rem] rounded-lg"
+        >
+            <h1 className="text-center leading-8 pb-2 font-syne text-3xl font-extrabold text-primary">
+                Iniciar Sesión
+            </h1>
+            <TextInput
+                type="text"
+                {...register("email", {
+                    required: "Debes introducir un email",
+                    pattern: {
+                        value: /[^@\s]+@[^@\s]+\.[^@\s]+/,
+                        message: "Introduce un email válido",
+                    },
+                })}
+                label="Email"
+                id="email"
+                placeholder={placeholders.email}
+                error={errors.email?.message}
+            />
+            <TextInput
+                type="password"
+                {...register("password", {
+                    required: "Debes introducir una contraseña"
+                })}
+                label="Contraseña"
+                id="password"
+                placeholder={placeholders.password}
+                error={errors.password?.message}
+            />
+            <Button
+                text="Iniciar Sesión"
+                type="submit"
+                color="primary"
+                styles="mt-5"
+            />
+        </form>
+    );
+};
+
+export default LogInForm;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,18 @@
+import Return from "../components/buttons/Return";
+import LogInForm from "../components/forms/LogInForm";
 
 const Login = () => {
-  return (
-    <div>
-      login
-    </div>
-  )
-}
+    return (
+        <section>
+            <img
+                src="./MovieNight-logo.svg"
+                alt="MovieNight"
+                className="mx-auto mt-6 mb-9"
+            />
+            <Return route="/" />
+            <LogInForm />
+        </section>
+    );
+};
 
-export default Login
+export default Login;
