ff0ab1d05f88f65189fd457ff119d33937a9a4d6
Laura Gil <laura.gilcara@gmail.com>
2024-09-09 11:16:33 +0200

    add alerts with modal component

--- src/components/modals/Alert.jsx
+++ src/components/modals/Alert.jsx
@@ -2,7 +2,7 @@ import Button from "../buttons/Button"
 
 const Alert = ( {message, onAccept, onCancel, showCancel}) => {
   return (
-    <div className="absolute h-screen inset-0 bg-[rgba(23,20,44,0.85)] z-1000 flex items-center justify-center mx-3">
+    <div className="absolute h-screen inset-0 bg-[#17142cd9] z-1000 flex items-center justify-center mx-3">
         <div className="w-full bg-secondary relative inline m-auto rounded-md py-10 px-6 text-center">
             <p>{ message }</p>
             <div className="flex gap-4 mt-8">
