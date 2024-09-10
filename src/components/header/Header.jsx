7e63a9ed2ec7a13388f9fa914d42133c950a2a0f
Laura Gil <laura.gilcara@gmail.com>
2024-09-10 10:10:45 +0200

    add header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,27 @@
+import NavIcon from "./NavIcon";
 
 const Header = () => {
+    const navLinks = [
+        ['/buscador', './assets/search_icon.svg', 'Buscador'],
+        ['/watchlist', './assets/watchlist_icon.svg','Watchlist'],
+        ['/que-vemos', './assets/popcorn_icon.svg', '¿Qué Vemos?'],
+        ['/perfiles', './assets/profiles_icon.svg', 'Perfiles'],
+    ];
+
+    
   return (
-    <div>
-      
-    </div>
+    <header className="pt-5 mb-6 lg:flex lg:justify-between">
+        <img src="./MovieNight-logo.svg" alt="MovieNight" className="mx-auto lg:mx-0"/>
+        <nav>
+            <ul className="flex justify-center gap-7 mt-4">
+                {navLinks.map(link => (
+                <li key={link[0]}>
+                <NavIcon link={link[0]} icon={link[1]} text={link[2]} />
+                </li>
+            ))}
+            </ul>
+        </nav>
+    </header>
   )
 }
 
--- src/router/RedirectAuth.jsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from "../customHooks/useAuth"; // Ajusta la ruta según tu estructura de carpetas
-import Search from "../pages/Search";
-import FrontPage from "../pages/FrontPage";
-
-export const RedirectAuth = () => {
-    const { isAuthenticated } = useAuth();
-
-    return isAuthenticated ? <Search /> : <FrontPage />;
-};
