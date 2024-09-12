abd000259e6447e9c58f474c7505432037da8719
Laura Gil <laura.gilcara@gmail.com>
2024-09-12 20:23:42 +0200

    add functionality to like button. When clicked sends post request with content and profile id

--- src/components/buttons/Like.jsx
+++ src/components/buttons/Like.jsx
@@ -1,15 +1,66 @@
+import { useContext } from "react";
+import { ProfileContext } from "../../context/profile/ProfileContext";
+import { URL_API_ADD_CONTENT_PROFILE } from "../../config/urls";
+import { requestHandler } from "../../services/requestHandler";
+import { useCookie } from "../../customHooks/useCookie";
 
 const Like = ({ liked, variant = "small", type, data }) => {
+    const { profile } = useContext(ProfileContext);
+    const token = useCookie("authToken");
+    const url = URL_API_ADD_CONTENT_PROFILE(profile);
 
     const handleAddClick = () => {
-        if(type.toLowerCase() === 'tv') {
-            console.log("agregar serie: " + data.id);
-            console.log(data)
-        } else if(type.toLowerCase() === 'movie') {
-            console.log("agregar peli: " + data.id);
-            console.log(data)
+        console.log(url);
+        let apiId, creators = "", director = "", title, originalTitle, releaseDate, length = 0, seasons = 0;
+
+        if (type.toLowerCase() === "tv") {
+            apiId = `tv-${data.id}`;
+            creators = data.created_by.map(({ name }) => name).join(' - ');
+            title = data.name;
+            originalTitle = data.original_name;
+            releaseDate = data.last_air_date;
+            seasons = data.number_of_seasons;
+        } else if (type.toLowerCase() === "movie") {
+            apiId = `movie-${data.id}`;
+            director = data.credits.crew
+                .filter(({ job }) => job === "Director")
+                .map(({ name }) => name)
+                .join(' - ');
+            title = data.title;
+            originalTitle = data.original_title;
+            releaseDate = data.release_date;
+            length = data.runtime;
         }
+
+        const body = {
+            apiId: apiId,
+            title: title,
+            original_title: originalTitle,
+            summary: data.overview,
+            director: director,
+            creator: creators,
+            release_date: releaseDate,
+            length: length,
+            seasons: seasons,
+            poster_path: data.poster_path,
+            backdrop_path: data.backdrop_path,
+            genres: data.genres.map(({ name }) => ({ name }))
+        };
+
+        console.log(body.genres)
+
+        apiRequest(body);
     };
+
+    const apiRequest = async (body) => {
+        try {
+            const response = await requestHandler(url, "POST", body, token);
+            console.log(response.message);
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
     const handleRemoveClick = () => {
         console.log("eliminar " + data);
     };
--- src/config/urls.js
+++ src/config/urls.js
@@ -1,6 +1,7 @@
 const API_BASE = "http://localhost:8080"; 
 const API_AUTH_BASE = `${API_BASE}/auth`;
 const API_ENDPOINTS_PROFILES = `${API_BASE}/profiles`;  
+const API_ENDPOINTS_CONTENT = `${API_BASE}/api/content`
 
 // API Auth Endpoints
 export const URL_API_SIGNIN = `${API_AUTH_BASE}/register`;
@@ -9,6 +10,7 @@ export const URL_API_LOGIN = `${API_AUTH_BASE}/login`;
 // API Endpoints
 export const URL_API_ADD_PROFILE= `${API_ENDPOINTS_PROFILES}/new-profile`;
 export const URL_API_GET_PROFILES= `${API_ENDPOINTS_PROFILES}/getProfiles`;
+export const URL_API_ADD_CONTENT_PROFILE = (id) => `${API_ENDPOINTS_CONTENT}/${id}/addToWatchlist`;
 
 // The Movie DB API
 export const PHOTO_BASE_URL = "https://image.tmdb.org/t/p/original/";
