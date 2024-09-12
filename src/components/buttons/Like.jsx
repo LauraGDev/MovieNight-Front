1140bafea69ddd48efdc6b6f34f92b35403e209c
Laura Gil <laura.gilcara@gmail.com>
2024-09-12 10:51:03 +0200

    merge pull request

--- src/components/buttons/Like.jsx
+++ src/components/buttons/Like.jsx
@@ -1,24 +1,34 @@
-
-
-const Like = ({ id, liked }) => {
+const Like = ({ id, liked, variant = "small" }) => {
     const handleAddClick = () => {
         console.log("añadir " + id);
-    }
+    };
     const handleRemoveClick = () => {
         console.log("eliminar " + id);
     };
 
+    const containerStyles =
+        variant === "big"
+            ? "flex items-center bg-orange-500 text-white px-5 py-3 cursor-pointer"
+            : "cursor-pointer absolute top-[0.313rem] right-[0.313rem]";
+
     return (
-        <figure
-            className="cursor-pointer absolute top-[0.313rem] right-[0.313rem]"
+        <button
+            className={containerStyles}
             onClick={liked ? handleRemoveClick : handleAddClick}
         >
             <img
-                src={liked ? "./assets/liked_icon.svg": "./assets/like_icon.svg"}
+                src={
+                    liked ? "./assets/liked_icon.svg" : "./assets/like_icon.svg"
+                }
                 alt={liked ? "Eliminar de watchlist" : "Añadir a watchlist"}
-
+                className="transition-all hover:brightness-90"
             />
-        </figure>
+            {variant === "large" && (
+                <span>
+                    {liked ? "Eliminar de watchlist" : "Añadir a watchlist"}
+                </span>
+            )}
+        </button>
     );
 };
 
