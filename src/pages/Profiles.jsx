44c16e9807b471a486bc8e278f3ea490acae1c1e
Laura Gil <laura.gilcara@gmail.com>
2024-09-13 09:53:56 +0200

    add logout button to profile section

--- src/pages/Profiles.jsx
+++ src/pages/Profiles.jsx
@@ -1,10 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../components/buttons/Button";
 
 const Profiles = () => {
-  return (
-    <div>
-      profiles
-    </div>
-  )
-}
+    const navigate = useNavigate();
+    const logout = () => {
+        document.cookie = 'authToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        document.cookie = 'user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+        navigate("/");
+    }
 
-export default Profiles
+    return (
+        <section className="mt-5">
+            <Button
+                text="Cerrar Sesión"
+                color="accent"
+                styles="h-10"
+                onClick={logout}
+            />
+        </section>
+    );
+};
+
+export default Profiles;
